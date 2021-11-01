f1a8f27a4d7a9f835eb9eba82a43691cb26190f0
abutoku <ystkz2021@gmail.com>
2021-11-01 20:15:07 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // クイズの配列
-const umiushi_quiz = [
+const umiushi = [
    {
       question: './img/img_00.jpg',
       answers: ['ミズタマウミウシ', 'シラウミウシ', 'シロウミウシ'],
@@ -221,13 +221,13 @@ const colors = [
    },
 ];
 
-//html作成のための配列
+//select部分（色選択）作成のための配列
 const select_color = [];
 for( let i = 0; i < colors.length; i++) {
    select_color.push('<option value="'+ colors[i].color_val +'">'+ colors[i].color +'</option>')
 };
 //selectの初期表示を配列の先頭に追加
-select_color.unshift('<option hidden>選択してください</option>');
+select_color.unshift('<option hidden>色を選択</option>');
 //selectタグにhtmlを渡す
 $('#color_select').html(select_color);
 
@@ -241,8 +241,36 @@ $('#name_in').hide();
 //クリアボタンを消しておく
 $('#imgclear_btn').hide();
 
-/////////////////////クイズの動き///////////////////////
+/////////////////////クイズの動き////////////////////////////////////
 
+////////ランダムな配列をつくる
+const quiz = Array.from(umiushi);//シャッフル用に配列ウミウシを複製
+
+
+//配列をランダムに並び替えるための関数
+function randomSelect(x) {
+   let rndQ = [];//抽出したものを受け取るための配列
+
+   while (x.length > 0) { //配列数が0になるまで
+
+      // 配列数からランダムな数字を抽出
+      const rndInt = Math.floor(Math.random() * x.length);
+
+      // ランダムな数字のインデックスを指定し配列に入れる
+      rndQ.push(x[rndInt]);
+      // 配列に入れたものは削除する
+      quiz.splice(rndInt, 1);
+   }
+   
+   return rndQ;//できあがった配列を返す
+}
+
+const rndQuiz = randomSelect(quiz);//引数に配列quizを渡して上記ランダム関数を実行
+console.log(rndQuiz);
+//ここまででquizをランダムに並び替えたrndQuizができあがる
+
+
+///////////////問題部分の初期状態を表示
 
 //結果判定を入れていく配列
 let result = [];
@@ -253,88 +281,130 @@ $('#quiz_number').text(`Question${result.length +1}`);
 $('#quiz_h2').text('このウミウシの名前は？');
 
 //問題の写真を表示
-$('#question').html('<img src="' + umiushi_quiz[0].question + '">');
-
+$('#question').html('<img src="' + rndQuiz[0].question + '">');
 
 //回答ボタン部分
-$('#answer_0').text(`${umiushi_quiz[0].answers[0]}`);
-$('#answer_1').text(`${umiushi_quiz[0].answers[1]}`);
-$('#answer_2').text(`${umiushi_quiz[0].answers[2]}`);
+$('#answer_0').text(`${rndQuiz[0].answers[0]}`);
+$('#answer_1').text(`${rndQuiz[0].answers[1]}`);
+$('#answer_2').text(`${rndQuiz[0].answers[2]}`);
 
 //キーを判別するためにvalueを割り当てる
 $('#answer_0').val(0); //valは文字列
 $('#answer_1').val(1); //valは文字列
 $('#answer_2').val(2); //valは文字列
 
-//resultボタンクローズ
+
+//result true（結果画面）クローズ
 $('#close_true').on('click', function () {
    $('#result_true').addClass('delete');
 });
-   
+//result false（結果画面）クローズ
 $('#close_false').on('click', function () {
    $('#result_false').addClass('delete');
 });
-   
-//結果判定
-$('button').on('click', function (e) {  //クリックイベントの値を取る
-   //console.log(e.target.value); //押したボタンのバリューをコンソールに表示
-   if (Number(e.target.value) === umiushi_quiz[result.length].correct) { //valueを数値化
-      $("#result_true").removeClass('delete');//一致していれば正解を表示
-      result.push('ok'); //配列resultに結果を追加
-      console.log('ok');
-   } else {   
-      $("#result_false").removeClass('delete');//一致していればはずれを表示
-      result.push('ng');//配列resultに結果を追加
-      console.log('ng');
-   }
-      //配列resultとquiz.lengthの数が一致したら
-      //if (result.length === umiushi_quiz.length) {
-      if (result.length === 2) {
-      
-      $('#quiz_number').hide();//ヘッダーを消す
-      $('#quiz_h2').text('終了！！！');//終了〜！
-      $('#answer_list').hide();//imgを消す
-      $('#question').hide();//ulを消す
-      
-      
-      //結果が'ok'のものだけを配列win_countsに入れる
-      const win_counts = (result.filter((x) => {
-         return x === 'ok';
-      })
-      );
-      //結果にokの数を表示
-      $('#result').text(`SCORE${win_counts.length}/${umiushi_quiz.length}`);
-      
-      //ギャラリーボタンを出す
-      $('#gallery_btn').show();
-      
-      
-   } else { //まだ問題が残っているとき
-
-      //第何問目かを更新
-         $('#quiz_number').text(`Question.${result.length + 1}`);
-      //問題の写真を更新
-      $('#question').html('<img src="' + umiushi_quiz[result.length].question + '">');
-   
-      //回答ボタン部分を更新
-      $('#answer_0').text(`${umiushi_quiz[result.length].answers[0]}`);
-      $('#answer_1').text(`${umiushi_quiz[result.length].answers[1]}`);
-      $('#answer_2').text(`${umiushi_quiz[result.length].answers[2]}`);
-      
-   }
-
+//result time up（結果画面）クローズ
+$('#close_timeup').on('click', function () {
+   $('#time_up').addClass('delete');
 });
 
+///////////////クイズスタートしてからの動き//////////////
 
-////////////////////////////図鑑の動き//////////////////
+$('.next_btn').on('click', function () {//goボタン、nextボタンが押されたら
+   $('#go_screen').addClass('delete'); //goスクリーンを消す
+
+   if (result.length < 2) { //問題が終了していれば反応しないようにする
+      
+   
+      //タイマー用の変数を定義（秒数）
+      let time = 5;
+   
+      //timeから１ずつ減らしていく関数
+      function countDown() {
+         time--;// 1引く
+         console.log(time); //コンソールにtimeを表示
+         $('#quiz_time').html(`${time}`); //画面にtimeを表示
+         if (time === 0) { //もしtimeが0になったら
+            clearInterval(countStop); //タイマーストップ
+            $('#time_up').removeClass('delete'); //時間切れ画面を表示
+            result.push('ng');//配列resultに結果を追加
+            count_check();//問題が終了しているかのチェック終了していなければ次の問題         
+         }
+      }
+      //一秒ごとにcountDown関数を発動
+      const countStop = setInterval(countDown, 1000);
+   
+      //問題が終了しているかのチェックする関数
+      function count_check() {
+      
+         if (result.length === 2) { //回答数と問題数が一致したら
+            clearInterval(countStop);//タイマー止める
+
+            $('#quiz_number').hide();//ヘッダーを消す
+            $('#quiz_time').hide();
+            $('#quiz_h2').text('終了！！！');//終了〜！
+            $('#answer_list').hide();//imgを消す
+            $('#question').hide();//ulを消す
+         
+         
+            //結果が'ok'のものだけを配列win_countsに入れる
+            const win_counts = (result.filter((x) => {
+               return x === 'ok';
+            })
+            );
+
+            //結果にokの数を表示
+            $('#result').text(`SCORE${win_counts.length}/${rndQuiz.length}`);
+         
+            //ギャラリーボタンを出す
+            $('#gallery_btn').show();
+         } else { //まだ問題が残っているとき
+         
+            //第何問目かを更新
+            $('#quiz_number').text(`Question.${result.length + 1}`);
+            //問題の写真を更新
+            $('#question').html('<img src="' + rndQuiz[result.length].question + '">');
+         
+            //回答ボタン部分を更新
+            $('#answer_0').text(`${rndQuiz[result.length].answers[0]}`);
+            $('#answer_1').text(`${rndQuiz[result.length].answers[1]}`);
+            $('#answer_2').text(`${rndQuiz[result.length].answers[2]}`);
+         }
+      
+      }
+   
+      ////////////結果判定
+      $('button').on('click', function (e) {  //クリックイベントの値を取る
+         //console.log(e.target.value); //押したボタンのバリューをコンソールに表示
+         clearInterval(countStop);
+         if (Number(e.target.value) === rndQuiz[result.length].correct) { //valueを数値化
+            $("#result_true").removeClass('delete');//一致していれば正解を表示
+            result.push('ok'); //配列resultに結果を追加
+            //console.log('ok');
+         } else {
+            $("#result_false").removeClass('delete');//一致していればはずれを表示
+            result.push('ng');//配列resultに結果を追加
+            //console.log('ng');
+         }
+         //問題が終了しているかのチェック終了していなければ次の問題
+         count_check();
+   
+      });
+   }
+});///////////////クイズスタートしてからの動きここまで//////////////
+
+
+
+
+////////////////////////////図鑑の動き////////////////////////////////////////
 
 //ギャラリーボタンが押されたら
 $('#gallery_btn').on('click', function () {
+   
    $('.wrapper').css('background', '#404040'); //背景色を変更
    $('.wrapper').css('color', '#fff'); //文字を白にする
    $('#quiz_h2').hide();//終了を消す
    $('#result').hide();//SCOREを消す
-   $('.wrapper').css('height','1600px');
+   $('.wrapper').css('height','2000px');
 
    //色選択を表示
    $('#color_select').show();
@@ -346,8 +416,8 @@ $('#gallery_btn').on('click', function () {
    const images = [];//画像表示用の配列を用意
 
    //umiushi_quizのimgをhtmlにして配列に入れる
-   for (let i = 0; i < umiushi_quiz.length; i++) {
-      images.push('<img src="' + umiushi_quiz[i].question + '" class="umiushi_img">');
+   for (let i = 0; i < umiushi.length; i++) {
+      images.push('<img src="' + umiushi[i].question + '" class="umiushi_img">');
    }
    //imgだけ入った配列を・・・ 
    $('#gallery_img').html(images).hide(); //htmlに入れて一度隠す
@@ -357,13 +427,14 @@ $('#gallery_btn').on('click', function () {
 
 //ウミウシの画像がクリックされたら
 $('body').on('click', '.umiushi_img', function () {
-   let indexNumber = $(this).index()
-   let i = umiushi_quiz[indexNumber].correct;
-   console.log(umiushi_quiz[indexNumber].answers[i]);
-   $('#info_name').removeClass('delete');
-   $('#name').text(`${umiushi_quiz[indexNumber].answers[i]}`);
+   let indexNumber = $(this).index()//クリックされた画像のインデックス番号を取得
+   let i = umiushi[indexNumber].correct;//指定したオブジェクトの正解名のindexを取得
+   console.log(umiushi[indexNumber].answers[i]);
+   $('#info_name').removeClass('delete');//モーダルを表示
+   $('#name').text(`${umiushi[indexNumber].answers[i]}`);//モーダル内に文字を追加
 });
 
+//ウミウシ名のモーダルを消す
 $('body').on('click', '#info_name', function () {
    $('#info_name').addClass('delete');
 });
@@ -375,7 +446,7 @@ $('body').change('#color_seek', function () {
    //選択された値を取得
    let sel_color = $('select').val();
    //値と同じものを配列に入れる
-   const selected = umiushi_quiz.filter((x) => {
+   const selected = umiushi.filter((x) => {
       return x.color === sel_color;
    });
    console.log(selected);
@@ -410,11 +481,11 @@ $('#name_in').keyup(function () { //キーが上がったら
 
    console.log(str);//入力されたもののカタカナ
 
-   const selected = umiushi_quiz.filter((x) => {
+   const selected = umiushi.filter((x) => {
       let i = x.correct; //正解の名前を見つける
-      let true_name = x.answers[i];  //正解の名前を変数に代入
-      if (true_name.includes(str)) {
-         return x;
+      let true_name = x.answers[i];  //正解のウミウシ名を変数に代入
+      if (true_name.includes(str)) { //ウミウシ名の中にstrが含まれていたらtrue
+         return x;//その配列をselextedに返す（入れる）
       }
    });
 
